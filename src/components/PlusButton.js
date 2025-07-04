5c4eedd1b723845d8e7e0c19d374127d340f2811
Zaid Alshamaa <zshamaa20@gmail.com>
2025-07-04 19:18:51 +0300

    style: edit PlusButton Size

--- src/components/PlusButton.js
+++ src/components/PlusButton.js
@@ -2,7 +2,7 @@ import { FiPlus } from "react-icons/fi";
 
 export default function PlusButton ({className}) {
   return(
-    <button className={`bg-[#089bab] hover:bg-[#047986] duration-300 text-white p-2 rounded-xl absolute bottom-[15px] right-[15px] md:hidden ${className}`}>
+    <button className={`bg-[#089bab] hover:bg-[#047986] duration-300 text-white p-3 rounded-xl absolute bottom-[15px] right-[15px] md:hidden ${className}`}>
       <FiPlus className="text-lg" />
     </button>
   );
