67b105deec6b7f96dbad8abc26f79ef63814b8bf
Zaid Alshamaa <zshamaa20@gmail.com>
2025-07-18 18:29:02 +0300

    add Loading and Modal components

--- src/pages/Medications/Medications.js
+++ src/pages/Medications/Medications.js
@@ -12,6 +12,9 @@ import axios from "axios";
 import PlusButton from "../../components/PlusButton";
 import "../../index.css";
 import deleteConfirm from "../../assets/deleteConfirm.jpg"
+import Loading from "../../components/Loading";
+import Modal from "../../components/Modal";
+import successImage from '../../assets/success.gif';
 
 export default function Medications() {
   const [cards, setCards] = useState([]);
@@ -22,6 +25,10 @@ export default function Medications() {
   const [image, setImage] = useState(null);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isBoxOpen, setIsBoxOpen] = useState(false);
+  const [boxMessage, setBoxMessage] = useState("");
+  const [boxImage, setBoxImage] = useState("");
 
   // useRef
   const medicationName = useRef();
@@ -52,7 +59,16 @@ export default function Medications() {
   return () => {
     document.body.style.overflow = "auto";  
   };
-}, [confirmDelete]);
+  }, [confirmDelete]);
+
+  useEffect(() => {
+    if (isBoxOpen) {
+      const timer = setTimeout(() => {
+        setIsBoxOpen(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [isBoxOpen]);
 
   // Mapping
   const imageShow = image ? (
@@ -69,28 +85,29 @@ export default function Medications() {
     }
   };
 
-const handleDrag = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  if (e.type === "dragenter" || e.type === "dragover") {
-    setDragActive(true);
-  } else if (e.type === "dragleave") {
-    setDragActive(false);
-  }
-};
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
 
-const handleDrop = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  setDragActive(false);
-  if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-    const file = e.dataTransfer.files[0]; // فقط أول ملف
-    setImage(file);
-    e.dataTransfer.clearData();
-  }
-};
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      const file = e.dataTransfer.files[0]; // فقط أول ملف
+      setImage(file);
+      e.dataTransfer.clearData();
+    }
+  };
 
   async function submit() {
+    setIsLoading(true);
     const formData = new FormData();
     formData.append("name", name)
     formData.append("info", description)
@@ -108,13 +125,21 @@ const handleDrop = (e) => {
         setName("");
         setDescription("");
         setImage(null);
+        setBoxMessage("The Medication Has Been Added Successfully !");
+        setBoxImage(successImage);
       } catch (err){
       console.log("Error !");
       console.log(err);
+      setBoxMessage("Something Went Wrong !");
+      setBoxImage(successImage);
+    } finally {
+      setIsLoading(false);
+      setIsBoxOpen(true);
     }
   }
 
   async function handleDelete() {
+    setIsLoading(true);
     try {
       await axios.delete( `${BaseUrl}/medication/${medicationId.current}`,
         {
@@ -125,8 +150,15 @@ const handleDrop = (e) => {
         });
         setConfirmDelete(false);
         setCount((prev) => prev + 1);
+        setBoxMessage("The Medication Has Been Deleted Successfully !");
+        setBoxImage(successImage);
     } catch {
       console.log("Error !");
+      setBoxMessage("Something Went Wrong !");
+      setBoxImage(successImage);
+    } finally {
+      setIsLoading(false);
+      setIsBoxOpen(true);
     }
   }
 
@@ -180,24 +212,29 @@ const handleDrop = (e) => {
         addBox &&
         <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center p-2">
           <div className="bg-white rounded-xl p-5 text-xl flex flex-col items-center shadow-xl w-[500px]">
-            <div className="text-right mb-5 w-full">
-              <h1 className="font-bold text-2xl text-center">أضف دواء</h1>
-              <div className="flex flex-col mb-3">
-                <label className="px-4 mb-2">اسم الدواء</label>
-                <input name="name"
+            <div className=" mb-5 w-full">
+              <h1 className="font-bold text-2xl text-center">Add Medication</h1>
+              <div className="flex flex-col my-3 font-semibold">
+                <label className="px-4 mb-2">Medication Name</label>
+                <input
+                name="name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 autoFocus
-                dir="rtl"
-                placeholder="أضف اسم الدواء"
-                className="outline-none border-2 border-transparent focus:border-[#089bab] bg-gray-100 rounded-xl text-right py-1 px-4" />
+                placeholder="Add medication name"
+                className="placeholder:text-base outline-none border-2 border-transparent focus:border-[#089bab] bg-gray-100 rounded-xl py-1 px-4"
+                />
               </div>
-              <div className="flex flex-col">
-                <label className="px-4 mb-2">وصف الدواء</label>
-                <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} dir="rtl" placeholder="أضف وصف عن الدواء" className="outline-none border-2 border-transparent focus:border-[#089bab] min-h-[100px] resize-none bg-gray-100 rounded-xl text-right py-1 px-4" />
+              <div className="flex flex-col font-semibold">
+                <label className="px-4 mb-2">Medication Description</label>
+                <textarea
+                name="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Descripe the mdeication"
+                className="placeholder:text-base outline-none border-2 border-transparent focus:border-[#089bab] min-h-[100px] resize-none bg-gray-100 rounded-xl py-1 px-4" />
               </div>
             </div>
-
             <div
               onDragEnter={handleDrag}
               onDragOver={handleDrag}
@@ -206,7 +243,6 @@ const handleDrop = (e) => {
               onDrop={handleDrop}
               className="grow relative flex items-center justify-center my-[10px] bg-transparent w-full md:w-[375px] h-[175px] md:h-[225px] border-2 border-dashed border-[#AEAEAE] rounded-[15px] self-center cursor-pointer"
             >
-
               <input
                 ref={inputImageRef}
                 hidden
@@ -217,23 +253,22 @@ const handleDrop = (e) => {
               <div className={`${image ? `hidden` : `flex flex-col items-center`}`}>
                 <i className="fa-solid fa-image text-[#7F7F7F] text-[30px] md:text-[60px] mb-[10px]"></i>
                 <p className="text-[12px] md:text-[14px]">
+                  Choose An Image {" "}
                   <span className="hidden md:inline-block">
-                    أو اسحبها هنا
+                    or Drag & Drop It
                   </span>{" "}
-                  اختر الصورة {" "}
                 </p>
               </div>
               {imageShow}
             </div>
-
             <div className="flex justify-center w-full mt-5">
-              <button className="w-[75px] bg-green-500 border-2 border-green-500 p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300 mr-7" onClick={() => submit()}>إضافة</button>
-              <button className="w-[75px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300" onClick={() => {
+              <button className="w-[85px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300" onClick={() => {
                 setAddBox(false);
                 setImage(null);
                 setName("");
                 setDescription("");
-              }}>تراجع</button>
+              }}>Cancel</button>
+              <button className="w-[85px] bg-[#089bab] border-2 border-[#089bab] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300 ml-7" onClick={() => submit()}>Add</button>
             </div>
           </div>
         </div>
@@ -244,16 +279,21 @@ const handleDrop = (e) => {
         <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center p-2">
           <div className="bg-white rounded-xl p-5 text-xl flex flex-col items-center shadow-xl w-[400px]">
             <img alt="image_delete" src={deleteConfirm} className="w-[200px]"/>
-            <p dir="rtl" className="my-5 text-center md:text-right">
-              هل تريد بالتأكيد حذف  {medicationName.current} ؟
+            <p className="my-5 text-center">
+              Do You Really Want To Delete {medicationName.current} ?
             </p>
             <div className="flex justify-center w-full">
-              <button className="w-[75px] bg-[#DD1015] border-2 border-[#DD1015] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300 mr-7" onClick={() => handleDelete()}>حذف</button>
-              <button className="w-[75px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300" onClick={() => setConfirmDelete(false)}>تراجع</button>
+              <button className="w-[85px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300" onClick={() => setConfirmDelete(false)}>Cancel</button>
+              <button className="w-[85px] bg-[#DD1015] border-2 border-[#DD1015] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300 ml-7" onClick={() => handleDelete()}>Delete</button>
             </div>
           </div>
         </div>
       }
+
+      {isLoading && <Loading />}
+
+      {isBoxOpen && <Modal message={boxMessage} imageSrc={boxImage}/>}
+
     </>
   );
 }
