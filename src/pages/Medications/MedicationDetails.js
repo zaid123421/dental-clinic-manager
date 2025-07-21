c6faf0579d7e7c3b83938de852414b88fa63f0ef
Zaid Alshamaa <zshamaa20@gmail.com>
2025-07-21 11:29:52 +0300

    feat: add edit medication feature

--- src/pages/Medications/MedicationDetails.js
+++ src/pages/Medications/MedicationDetails.js
@@ -1,21 +1,30 @@
+// import hooks
 import { useEffect, useState } from "react";
+// import components
 import Sidebar from "../../components/Sidebar";
 import Title from "../../components/Title";
+// import axios
 import axios from "axios";
-import { BaseUrl } from "../../config";
+// import backend configuration
+import { BaseUrl, ImageUrl } from "../../config";
+// import useNavigate tools
 import { useLocation, useNavigate } from "react-router-dom";
+// import icons
 import { IoIosArrowBack } from "react-icons/io";
 
 
 export default function MedicationDetails () {
+  // States
   const [details, setDetails] = useState({
     name: "",
     description: "",
     img: ""
   });
 
+  // useNavigate
   const nav = useNavigate();
 
+  // useLocation
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const mdeicationId = params.get('medicationId');
@@ -51,23 +60,24 @@ export default function MedicationDetails () {
           <Title className="flex-1" label={`${details.name} Details`} />
         </div>
 
-          <div
-          style={{ boxShadow: "0px 15px 20px 5px rgba(0, 0, 0, 0.25)" }}
-          className="flex flex-col items-center lg:items-start lg:flex-row p-5 bg-white mt-[25px] rounded-2xl justify-between text-base md:text-xl text-center md:text-left">
-            <div>
-              <div className="flex flex-col md:flex-row mb-[25px]">
-                <p className="font-bold md:mr-[25px] w-[100px] sm:w-[200px] md:w-[300px]">Medication Name:</p>
-                <p>{details.name}</p>
-              </div>
-              <div className="flex flex-col md:flex-row">
-                <p className="font-bold md:mr-[25px] w-[100px] sm:w-[200px] md:w-[300px]">Medication Description:</p>
-                <p>{details.description}</p>
-              </div>
+        <div
+        style={{ boxShadow: "0px 15px 20px 5px rgba(0, 0, 0, 0.25)" }}
+        className="flex flex-col items-center lg:items-start lg:flex-row p-5 bg-white mt-[25px] rounded-2xl justify-between text-base md:text-xl text-center md:text-left">
+          <div>
+            <div className="flex flex-col md:flex-row mb-[25px]">
+              <p className="font-extrabold md:mr-[25px] w-[100px] sm:w-[200px] md:w-[300px]">Medication Name:</p>
+              <p className="font-semibold">{details.name}</p>
             </div>
-            <div className="mt-[25px] lg:mt-0 lg:ml-[50px] w-fit lg:w-[500px]">
-              <img alt="medication_image" src={`http://127.0.0.1:8000/${details.img}`} className="rounded-2xl" />
+            <div className="flex flex-col md:flex-row">
+              <p className="font-extrabold md:mr-[25px] w-[100px] sm:w-[200px] md:w-[300px]">Medication Description:</p>
+              <p className="font-semibold">{details.description}</p>
             </div>
           </div>
+          <div className="mt-[25px] lg:mt-0 lg:ml-[50px] w-fit lg:w-[500px]">
+            <img alt="medication_image" src={`${ImageUrl}${details.img}`} className="rounded-2xl" />
+          </div>
+        </div>
+
       </div>
     </>
   );
--- src/pages/Medications/Medications.js
+++ src/pages/Medications/Medications.js
@@ -1,29 +1,39 @@
-import Button from "../../components/Button";
-import { IoIosSearch } from "react-icons/io";
-import { FiPlus } from "react-icons/fi";
-import FormInput from "../../components/FormInput";
-import Title from "../../components/Title";
-import Sidebar from "../../components/Sidebar";
-import { MdEdit } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
+//import hooks
 import { useEffect, useRef, useState } from "react";
-import { BaseUrl, ImageUrl } from "../../config";
-import axios from "axios";
-import PlusButton from "../../components/PlusButton";
-import "../../index.css";
-import deleteConfirm from "../../assets/deleteConfirm.jpg"
+// import components
+import Button from "../../components/Button";
+import FormInput from "../../components/FormInput";
+import Sidebar from "../../components/Sidebar";
 import Loading from "../../components/Loading";
 import Modal from "../../components/Modal";
+import PlusButton from "../../components/PlusButton";
+import Title from "../../components/Title";
+// import icons
+import { IoIosSearch } from "react-icons/io";
+import { FiPlus } from "react-icons/fi";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+// import backend configuration
+import { BaseUrl, ImageUrl } from "../../config";
+// import axios
+import axios from "axios";
+// import images
+import deleteConfirm from "../../assets/deleteConfirm.jpg"
 import successImage from '../../assets/success.gif';
 import { useNavigate } from "react-router-dom";
+// import style file
+import "../../index.css";
 
 export default function Medications() {
+  // States
   const [cards, setCards] = useState([]);
   const [confirmDelete, setConfirmDelete] = useState(false);
   const [addBox, setAddBox] = useState(false);
+  const [editBox, setEditBox] = useState(false);
   const [count, setCount] = useState(0);
   const [dragActive, setDragActive] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [id, setId] = useState(null);
   
   const [modal, setModal] = useState({
     isOpen: false,
@@ -35,15 +45,23 @@ export default function Medications() {
   name: "",
   description: "",
   image: null,
-});
+  });
+
+  const [oldMedicationForm, setOldMedicationForm] = useState({
+  name: "",
+  description: "",
+  image: null,
+  });
 
   // useRef
   const medicationName = useRef();
   const medicationId = useRef();
   const inputImageRef = useRef(null);
 
+  // useNavigate
   const nav = useNavigate();
 
+  // useEffect
   useEffect(() => {
     axios
       .get(`${BaseUrl}/medication`, {
@@ -80,14 +98,53 @@ export default function Medications() {
   }, [modal.isOpen]);
 
   // Mapping
-  const imageShow = medicationForm.image ? (
-  <img
-    className="w-full h-full rounded-xl"
-    src={URL.createObjectURL(medicationForm.image)}
-    alt="Uploaded"
-  />
-) : null;
+  const imageShow = medicationForm.image
+  ? typeof medicationForm.image === "string"
+    ? medicationForm.image
+    : URL.createObjectURL(medicationForm.image)
+  : null;
 
+  const showCards = cards.map((card, index) => (
+    <div key={index} onClick={() => nav(`/medication-details?medicationId=${card.id}`)} className="cursor-pointer shadow-xl bg-white rounded-xl p-4 flex flex-col justify-between text-center">
+      <div className="bg-blue-300 rounded-md h-[150px] md:h-[125px] bg-contain">
+        <img
+          className="rounded-md w-full h-full"
+          src={`${ImageUrl}${card.image}`}
+          alt="medication_image"
+        />
+      </div>
+      <p className="my-3 font-bold">{card.name}</p>
+      <div className="flex text-2xl justify-center">
+        <div onClick={(e) => {
+          setOldMedicationForm({
+            name: card.name,
+            description: card.info,
+            image: `${ImageUrl}${card.image}`
+          })
+          setMedicationForm({
+            name: card.name,
+            description: card.info,
+            image: `${ImageUrl}${card.image}`
+          })
+          setId(card.id)
+          setEditBox(true);
+          e.stopPropagation();
+        }} className="bg-[#089bab] p-1 mr-2 text-white rounded-lg md:rounded-xl border-2 border-[#089bab] hover:bg-transparent hover:text-black transition duration-300 cursor-pointer">
+          <MdEdit className="text-sm md:text-base" />
+        </div>
+        <div onClick={(e) => {
+          e.stopPropagation();
+          medicationName.current = card.name;
+          medicationId.current = card.id;
+          setConfirmDelete(true);
+        }} className="bg-red-500 p-1 text-white rounded-lg md:rounded-xl border-2 border-red-500 hover:bg-transparent hover:text-black transition duration-300 cursor-pointer">
+          <MdDelete className="text-sm md:text-base" />
+        </div>
+      </div>
+    </div>
+  ));
+
+  // Functions
   const handleImageClick = () => {
     if (inputImageRef.current) {
       inputImageRef.current.click();
@@ -109,8 +166,11 @@ export default function Medications() {
     e.stopPropagation();
     setDragActive(false);
     if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      const file = e.dataTransfer.files[0]; // فقط أول ملف
-      medicationForm.image(file);
+      const file = e.dataTransfer.files[0]; 
+      setMedicationForm((prev) => ({
+      ...prev,
+      image: file,
+    }));
       e.dataTransfer.clearData();
     }
   };
@@ -152,6 +212,49 @@ export default function Medications() {
     }
   }
 
+  async function edit() {
+    setIsLoading(true);
+    const formData = new FormData();
+    if(medicationForm.name !== oldMedicationForm.name) {
+    formData.append("name", medicationForm.name);
+    }
+    formData.append("info", medicationForm.description);
+    if (medicationForm.image && typeof medicationForm.image !== "string") {
+      formData.append("image", medicationForm.image);
+    }
+    formData.append("_method", "patch")
+    try {
+      await axios.post( `${BaseUrl}/medication/${id}`, formData,
+        {
+          headers: {
+            Accept: "application/json",
+            // Authorization: `Bearer ${token}`,
+          },
+        });
+        setEditBox(false);
+        setCount((prev) => prev + 1);
+        setMedicationForm({
+          name: "",
+          description: "",
+          image: null,
+        });
+        setModal({
+          isOpen: true,
+          message: "The Medication Has Been Edited Successfully !",
+          image: successImage,
+      });
+      } catch (err){
+        console.log(err)
+      setModal({
+        isOpen: true,
+        message: "Something Went Wrong !",
+        image: successImage,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   async function handleDelete() {
     setIsLoading(true);
     try {
@@ -179,32 +282,6 @@ export default function Medications() {
       setIsLoading(false);
     }
   }
-
-  const showCards = cards.map((card, index) => (
-    <div key={index} onClick={() => nav(`/medication-details?medicationId=${card.id}`)} className="cursor-pointer shadow-xl bg-white rounded-xl p-4 flex flex-col justify-between text-center">
-      <div className="bg-blue-300 rounded-md h-[150px] md:h-[125px] bg-contain">
-        <img
-          className="rounded-md w-full h-full"
-          src={`${ImageUrl}${card.image}`}
-          alt="medication_image"
-        />
-      </div>
-      <p className="my-3 font-bold">{card.name}</p>
-      <div className="flex text-2xl justify-center">
-        <div className="bg-[#089bab] p-1 mr-2 text-white rounded-lg md:rounded-xl border-2 border-[#089bab] hover:bg-transparent hover:text-black transition duration-300 cursor-pointer">
-          <MdEdit className="text-sm md:text-base" />
-        </div>
-        <div onClick={(e) => {
-          e.stopPropagation();
-          medicationName.current = card.name;
-          medicationId.current = card.id;
-          setConfirmDelete(true);
-        }} className="bg-red-500 p-1 text-white rounded-lg md:rounded-xl border-2 border-red-500 hover:bg-transparent hover:text-black transition duration-300 cursor-pointer">
-          <MdDelete className="text-sm md:text-base" />
-        </div>
-      </div>
-    </div>
-  ));
 
   return (
     <>
@@ -253,7 +330,7 @@ export default function Medications() {
                 <label className="px-4 mb-2">Medication Description</label>
                 <textarea
                 name="description"
-                value={medicationName.description}
+                value={medicationForm.description}
                 onChange={(e) =>
                 setMedicationForm((prev) => ({
                   ...prev,
@@ -292,7 +369,9 @@ export default function Medications() {
                   </span>{" "}
                 </p>
               </div>
-              {imageShow}
+              {imageShow && (
+                <img className="w-full h-full rounded-xl" src={imageShow} alt="Uploaded" />
+              )}
             </div>
             <div className="flex justify-center w-full mt-5">
               <button className="w-[85px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300" onClick={() => {
@@ -325,10 +404,93 @@ export default function Medications() {
         </div>
       }
 
+      {
+        editBox &&
+        <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center p-2">
+          <div className="bg-white rounded-xl p-5 text-xl flex flex-col items-center shadow-xl w-[500px]">
+            <div className=" mb-5 w-full">
+              <h1 className="font-bold text-2xl text-center">Edit Mdeication</h1>
+              <div className="flex flex-col my-3 font-semibold">
+                <label className="px-4 mb-2">Medication Name</label>
+                <input
+                name="name"
+                value={medicationForm.name}
+                onChange={(e) =>
+                  setMedicationForm((prev) => ({
+                    ...prev,
+                    name: e.target.value,
+                  }))
+                }
+                autoFocus
+                placeholder="Add medication name"
+                className="placeholder:text-base outline-none border-2 border-transparent focus:border-[#089bab] bg-gray-100 rounded-xl py-1 px-4"
+                />
+              </div>
+              <div className="flex flex-col font-semibold">
+                <label className="px-4 mb-2">Medication Description</label>
+                <textarea
+                name="description"
+                value={medicationForm.description}
+                onChange={(e) =>
+                setMedicationForm((prev) => ({
+                  ...prev,
+                  description: e.target.value,
+                }))
+              }
+                placeholder="Descripe the mdeication"
+                className="placeholder:text-base outline-none border-2 border-transparent focus:border-[#089bab] min-h-[100px] resize-none bg-gray-100 rounded-xl py-1 px-4" />
+              </div>
+            </div>
+            <div
+              onDragEnter={handleDrag}
+              onDragOver={handleDrag}
+              onDragLeave={handleDrag}
+              onClick={handleImageClick}
+              onDrop={handleDrop}
+              className="grow relative flex items-center justify-center my-[10px] bg-transparent w-full md:w-[375px] h-[175px] md:h-[225px] border-2 border-dashed border-[#AEAEAE] rounded-[15px] self-center cursor-pointer"
+            >
+              <input
+                ref={inputImageRef}
+                hidden
+                type="file"
+                onChange={(e) =>
+                setMedicationForm((prev) => ({
+                  ...prev,
+                  image: e.target.files[0],
+                }))}
+              />
+
+              <div className={`${medicationForm.image ? `hidden` : `flex flex-col items-center`}`}>
+                <i className="fa-solid fa-image text-[#7F7F7F] text-[30px] md:text-[60px] mb-[10px]"></i>
+                <p className="text-[12px] md:text-[14px]">
+                  Choose An Image {" "}
+                  <span className="hidden md:inline-block">
+                    or Drag & Drop It
+                  </span>{" "}
+                </p>
+              </div>
+              {imageShow && (
+                <img className="w-full h-full rounded-xl" src={imageShow} alt="Uploaded" />
+              )}
+            </div>
+            <div className="flex justify-center w-full mt-5">
+              <button onClick={() => {
+                setEditBox(false);
+                setMedicationForm({
+                name: "",
+                description: "",
+                image: null,
+              });
+              }} className="w-[85px] bg-[#9e9e9e] border-2 border-[#9e9e9e] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300">Cancel</button>
+              <button onClick={() => edit()} className="w-[85px] bg-[#089bab] border-2 border-[#089bab] p-1 rounded-xl text-white hover:bg-transparent hover:text-black duration-300 ml-7">Edit</button>
+            </div>
+          </div>
+        </div>
+      }
+
       {isLoading && <Loading />}
 
       {modal.isOpen && <Modal message={modal.message} imageSrc={modal.image}/>}
-
     </>
   );
 }
